51e669fcc3ebf88414ed2f7286ffc6845fa4807d
Stefan Lissia <stefan.emailacct@gmail.com>
2016-03-02 22:57:47 +0000

    setup select options

--- assets/calculator.js
+++ assets/calculator.js
@@ -3,7 +3,9 @@
 	var app = angular.module('myCalculator', []);
 
 	app.controller('CalculatorController',['$scope', function($scope) {
-		alert('it works');
+		$scope.lumen_options = [375, 600, 900, 1125, 1600];
+		$scope.current_lumens = 600;
+		
 
 	}]);
 
